dfb84614571294ac7414809b12a57d9bacfdfbcc
pnocera <pnocera@gmail.com>
2023-08-21 15:38:32 +0200

    gohome

--- 00-build.sh
+++ 00-build.sh
@@ -1,3 +1,3 @@
-docker build . -t registry.joybook.io/joyb-dex:v0.0.2
+docker build . -t registry.joybook.io/joyb-dex:v0.0.3
 
-docker push registry.joybook.io/joyb-dex:v0.0.2
+docker push registry.joybook.io/joyb-dex:v0.0.3
